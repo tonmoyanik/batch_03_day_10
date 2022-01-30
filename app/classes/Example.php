695aff21dce0ac4ebe06f867c2a86e84f0044df4
tonmoyanik <poizongreen007@gmail.com>
2022-01-30 13:37:40 +0600

    updated

---
+++ app/classes/Example.php
@@ -0,0 +1,150 @@
+<?php
+namespace App\classes;
+class Example
+{
+    public $name;
+    public $email;
+    public $mobile;
+    public $firstName;
+    public $lastName;
+    public $x;
+    public $y;
+    public $z;
+    public $data= [];
+
+    public function index()
+    {
+       $this->data = [10, 20, 30, 'BITM', 100.200, true];
+      // echo $this->data[9];
+
+
+
+
+
+
+//       do {
+//           echo 'Hello!<br/>';
+//           $this->x++;
+//       }
+//       while($this->x < 110);
+
+
+//       while ($this->x < 110)
+//       {
+//           echo 'Hello!<br/>';
+//           $this->x++;
+//       }
+
+
+
+
+
+
+//        for ($this->y= 99; $this->y >= 76; $this->y--)
+//        {
+//            echo $this->y. ' ';
+//        }
+
+
+
+
+
+//      switch ($this->x)
+//      {
+//          case 10:
+//              echo 'Hello!';
+//              break;
+//          case 20:
+//            echo 'Bangladesh';
+//            break;
+//          case 30:
+//              echo 'Dhaka';
+//              break;
+//          default:
+//              echo 'BITM';
+//
+//
+//
+//      }
+
+
+//        if($this->x >20)
+//      {
+//            echo $this->x;
+//        }
+//        else
+//        {
+//            echo 'Hello!';
+//        }
+
+
+//        if($this->x >20)
+//        {
+//            echo $this->x;
+//        }
+//        $this->y = '20';
+//        $this->z = '30';
+
+
+
+//        echo ($this->x > $this->y) || ($this->y > $this->z);
+//        echo '<br/>....<br/>';
+//        echo ($this->x > $this->y) || ($this->y < $this->z);
+//        echo '<br/>....<br/>';
+//        echo ($this->x < $this->y) || ($this->y > $this->z);
+//        echo '<br/>....<br/>';
+//        echo ($this->x < $this->y) || ($this->y < $this->z);
+//        echo '<br/>....<br/>';
+
+
+//        echo $this->x > $this->y;
+//        echo '<br/>';
+//        echo $this->x >= $this->y;
+//        echo '<br/>';
+//        echo $this->x < $this->y;
+//        echo '<br/>';
+//        echo $this->x <= $this->y;
+//        echo '<br/>';
+//        echo $this->x == $this->y;
+//        echo '<br/>';
+//        echo $this->x != $this->y;
+//        echo '<br/>';
+//        echo $this->x === $this->y;
+//        echo '<br/>';
+//        echo $this->x !== $this->y;
+
+
+
+
+//        echo $this->x += $this->y;
+//        echo '<br/>';
+//        echo $this->x -= $this->y;
+//        echo '<br/>';
+//        echo $this->x *= $this->y;
+//        echo '<br/>';
+//        echo $this->x /= $this->y;
+//        echo '<br/>';
+//        echo $this->x %= $this->y;
+//        echo '<br/>';
+//        echo $this->x .= $this->y;
+//        echo '<br/>';
+//        echo $this->x %= $this->y;
+
+
+
+
+
+//        echo $this->x + $this->y;
+//        echo '<br/>';
+//        echo $this->x - $this->y;
+//        echo '<br/>';
+//        echo $this->x * $this->y;
+//        echo '<br/>';
+//        echo $this->x / $this->y;
+//        echo '<br/>';
+//        echo $this->x % $this->y;
+
+
+
+    }
+}
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+    require_once 'vendor/autoload.php';
+    use App\classes\Example;
+
+    $example = new Example();
+    $example->index();
